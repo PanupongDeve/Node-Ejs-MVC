ad2f0014e33d7d3218c0d0c49c82667492c653d1
PanupongDeve <5635512110@psu.ac.th>
2018-08-10 22:12:53 +0700

    updated

--- DesignLayer/Database/mongo/Mongo.js
+++ DesignLayer/Database/mongo/Mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost:27017/cats';
+const mongoURI = 'mongodb://mongo:27017/cats';
 const mongoTestURI = ''
 
 module.exports = class Mongo {
